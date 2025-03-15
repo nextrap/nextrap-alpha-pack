555eba7ef2f8d0ed508b9ffb0f9ea02de29b3c28
Marcel Frank <M.Frank@Decadis.de>
2025-03-15 15:54:51 +0100

    image-component: first version of slider; add fullsize feature

--- src/image/doc.ts
+++ src/image/doc.ts
@@ -10,35 +10,24 @@ registerComponent({
             description: "A simple image",
             lang: "html",
             code: `
-<nxa-image style="width: 600px; height: 600px">
+<nxa-image style="width: 600px; height: 600px; margin-bottom: 10em" data-features="fullsize">
     <img src="src/image/pixel-matrix.png" style="object-position: center center;">
 </nxa-image>
-            `
-        },
-        {
-            title: "Cropped Image",
-            description: "A simple image cropped",
-            lang: "html",
-            code: `
-            <br/><br/>
-            <div style="display: flex; height: 400px">
-<nxa-image style="width: 300px; height: 600px">
-    <img style="object-position: left top;" data-crop="top: 0%; bottom: 0%; right: 0%; left: 0%" src="/src/image/pixel-matrix.png">
-</nxa-image>
-<nxa-image style="">
-    <img style="object-position: left top;" data-crop="top: 0px; bottom: 0px; right: 0px; left: 0px" src="/src/image/pixel-matrix.png">
+
+<h3>Using data-crop</h3>
+<div style="display: flex; height: 400px; overflow: hidden; margin-bottom: 10em">
+    <nxa-image style="width: 300px; height: 600px">
+        <img style="object-position: left top;" data-crop="top: 0%; bottom: 0%; right: 0%; left: 0%" src="/src/image/pixel-matrix.png">
+    </nxa-image>
+    <nxa-image>
+        <img style="object-position: left top;" data-crop="top: 0px; bottom: 0px; right: 0px; left: 0px" src="/src/image/pixel-matrix.png">
 </nxa-image>
 </div>
-            `
-        },
-        {
-            title: "Slideshow",
-            description: "Eine Slideshow",
-            lang: "html",
-            code: `
-<div style="display:none">
+
+<h3>Slideshow</h3>
+<div>
 <nxa-image style="width: 100%; height: 250px" data-features="slideshow blend fullsize">
-    <img src="https://cdn.leuffen.de//leu-kdnlogos/v2/20/267-128_267/MVZ-GG.svg" style="object-position: top center;" data-caption="Bild 1" data-crop="top: 10%; bottom: 20%">
+    <img src="https://cdn.leuffen.de//leu-kdnlogos/v2/20/267-128_267/MVZ-GG.svg" style="object-position: top center;" data-caption="Bild 1">
     <img src="https://cdn.leuffen.de//leu-kdnlogos/v2/19/85-64_170/Praxis-Gromes.svg" style="object-position: bottom center;">
     <img src="https://cdn.leuffen.de//leu-kdnlogos/v2/18/57-32_228/Gefaess-Gallis.svg" style="object-position: center center;">
 </nxa-image>
--- src/image/nxa-image.ts
+++ src/image/nxa-image.ts
@@ -1,4 +1,5 @@
 import {cropImage, cssToJson} from "./nxa-image.utils";
+import {defaultSlideshowInterval, SlideShowConfig, SlideShowTransitions} from "./nxa-image.types";
 
 const style = `
 :host {
@@ -21,7 +22,9 @@ class NxaImage extends HTMLElement {
     private container: HTMLElement;
     private globalDataCrop: Record<string, string>;
     private childDataCrop: Record<string, string>[] = [];
-    private referenceSize: { width: string; height: string };
+    private dataFeatures: string[];
+    private slidesShowConfig: SlideShowConfig = {};
+    private fullSize: boolean; // defines, if it's possible to show the image in full size on click
 
     constructor() {
         super();
@@ -51,6 +54,61 @@ class NxaImage extends HTMLElement {
         for (let child of Array.from(this.children)) {
             this.childDataCrop.push(cssToJson(child?.getAttribute("data-crop") || ""));
         }
+
+        this.dataFeatures = this.getAttribute("data-features")?.split(" ") || [];
+        this.initSlidesShowConfig();
+
+        this.fullSize = this.dataFeatures.includes("fullsize");
+        this.initFullSize();
+    }
+
+    initFullSize() {
+        if (this.fullSize) {
+            for (let child of Array.from(this.children)) {
+                child.style.cursor = "pointer";
+                child.addEventListener("click", () => {
+                    const img: HTMLImageElement = this.querySelector("img");
+                    if (img) {
+                        const fullSizeImg = document.createElement("img");
+                        fullSizeImg.src = img.src;
+                        fullSizeImg.style.position = "fixed";
+                        fullSizeImg.style.top = "0";
+                        fullSizeImg.style.left = "0";
+                        fullSizeImg.style.width = "100%";
+                        fullSizeImg.style.height = "100%";
+                        fullSizeImg.style.objectFit = "contain";
+                        fullSizeImg.style.backgroundColor = "rgba(0, 0, 0, 0.8)";
+                        fullSizeImg.style.zIndex = "1000";
+                        fullSizeImg.style.cursor = "zoom-out";
+                        fullSizeImg.addEventListener("click", () => {
+                            fullSizeImg.remove();
+                        });
+
+                        document.addEventListener("keydown", (event) => {
+                            if (event.key === "Escape") {
+                                fullSizeImg.remove();
+                            }
+                        });
+
+                        document.body.appendChild(fullSizeImg);
+                    }
+                });
+            }
+        }
+    }
+
+    initSlidesShowConfig() {
+        if(!this.dataFeatures) {
+            this.dataFeatures = this.getAttribute("data-features")?.split(" ") || [];
+        }
+        this.slidesShowConfig.enabled = this.dataFeatures.includes("slideshow");
+        this.slidesShowConfig.interval = defaultSlideshowInterval;
+        this.slidesShowConfig.pauseOnHover = this.dataFeatures.includes("pauseOnHover");
+        this.slidesShowConfig.transition = SlideShowTransitions.filter(t => this.dataFeatures.includes(t))[0] || "fade";
+        this.slidesShowConfig.showThumbnails = this.dataFeatures.includes("showThumbnails");
+        this.slidesShowConfig.showArrows = this.dataFeatures.includes("showArrows");
+        this.slidesShowConfig.showIndicators = this.dataFeatures.includes("showIndicators");
+        this.slidesShowConfig.showCaptions = this.dataFeatures.includes("showCaptions");
     }
 
     connectedCallback() {
@@ -60,10 +118,17 @@ class NxaImage extends HTMLElement {
         console.log("this.globalDataCrop", this.globalDataCrop);
         console.log("this.childDataCrop", this.childDataCrop);
         console.log("this.children", this.children);
+        console.log("this.slidesShowConfig", this.slidesShowConfig);
         console.groupEnd();
 
         // Initially set up styles
         this.setDefaultStyles();
+
+        if (this.slidesShowConfig.enabled) {
+            this.attachSlideshowStyles();
+            this.attachSlideshowAnimations();
+            this.initSlideshowInterval();
+        }
 
         // Wait for layout to complete before measuring
         requestAnimationFrame(() => {
@@ -77,13 +142,102 @@ class NxaImage extends HTMLElement {
         });
     }
 
+    /**
+     * Attaches animations for the slideshow.
+     * Take a look at the SlideShowConfig to see which animations are enabled.
+     */
+    attachSlideshowAnimations() {
+        const transition = this.slidesShowConfig.transition || "fade";
+
+        // Add style element to shadow DOM for animations
+        const styleElement = document.createElement('style');
+        styleElement.textContent = `
+            ::slotted(img) {
+                transition: opacity 0.5s ease;
+            }
+
+            ::slotted(img.active) {
+                opacity: 1;
+                z-index: 1;
+            }
+
+            ${transition === "blend" ? `
+            ::slotted(img.active) {
+                animation: blendTransition 0.5s ease;
+            }
+            @keyframes blendTransition {
+                from { opacity: 0.5; }
+                to { opacity: 1; }
+            }
+            ` : ''}
+        `;
+
+        this.shadowRoot.appendChild(styleElement);
+
+        // Add controls if necessary
+        if (this.slidesShowConfig.showArrows) {
+            this.addNavigationArrows();
+        }
+
+        if (this.slidesShowConfig.showIndicators) {
+            this.addIndicators();
+        }
+    }
+
+    nextSlide() {
+        const activeSlide = this.querySelector("img.active");
+        const nextSlide = activeSlide.nextElementSibling || this.querySelector("img:first-child");
+        activeSlide.classList.remove("active");
+        nextSlide.classList.add("active");
+    }
+
+    prevSlide() {
+        const activeSlide = this.querySelector("img.active");
+        const prevSlide = activeSlide.previousElementSibling || this.querySelector("img:last-child");
+        activeSlide.classList.remove("active");
+        prevSlide.classList.add("active");
+    }
+
+    initSlideshowInterval() {
+        const interval = this.slidesShowConfig.interval || defaultSlideshowInterval;
+        setInterval(() => {
+            this.nextSlide();
+        }, interval);
+    }
+
+    attachSlideshowStyles() {
+        console.group("attachSlideshowStyles");
+        console.log("this.children[0]", this.children[0]);
+        console.groupEnd();
+
+        // add "active" attribute to first child
+        if (this.children[0]) {
+            this.children[0].classList.add("active");
+        }
+
+        // add "slideshow" class to container
+        this.classList.add("slideshow");
+
+        // Add style element to document head for global styles
+        const globalStyle = document.createElement('style');
+        globalStyle.textContent = `
+    nxa-image.slideshow img:not(.active) {
+        opacity: 0;
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+    }
+  `;
+        document.head.appendChild(globalStyle);
+    }
+
     setDefaultStyles() {
         for (let child of Array.from(this.children)) {
             if (!child.hasAttribute(style)) {
-                console.log("has it");
                 child.getAttribute(style);
             }
-            console.log("child", child);
         }
     }
 
@@ -109,8 +263,6 @@ class NxaImage extends HTMLElement {
                 height: this.offsetHeight + 'px'
             };
 
-            console.log("referenceSize", referenceSize);
-            console.log("cropData", cropData);
             cropImage(child, cropData, referenceSize);
         });
     }
